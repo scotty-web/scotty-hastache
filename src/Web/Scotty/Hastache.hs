497e0fc50128579771fb5f81ccde63c78cbc0026
Daniil Frumin <difrumin@gmail.com>
2013-08-09 12:24:42 +0400

    Slight cleanup

--- src/Web/Scotty/Hastache.hs
+++ src/Web/Scotty/Hastache.hs
@@ -1,40 +1,38 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
-{-# LANGUAGE TypeHoles #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
 
 -- | Hastache templating for Scotty
 module Web.Scotty.Hastache where
 
-import Data.Maybe
-import Data.Map (Map)
-import qualified Data.Map as M
-import Data.Monoid
-import Control.Monad.Morph
-import Blaze.ByteString.Builder (fromByteString)
-import Web.Scotty as S
-import Web.Scotty.Types
-import Network.Wai
-import Network.Wai.Handler.Warp (Port, runSettings, settingsPort)
-import Network.HTTP.Types
-import Data.Default
-import Control.Monad.State as State
-import Text.Hastache
-import Text.Hastache.Context
-import qualified Data.ByteString.Lazy as BL
-import System.FilePath.Posix
-import Text.Blaze.Internal
-import Text.Blaze.Html.Renderer.Utf8 as BRU
-import Text.Blaze.Html.Renderer.String as BRS
+import           Blaze.ByteString.Builder        (fromByteString)
+import           Control.Monad.Morph
+import           Control.Monad.State             as State
+import           Data.Default
+import           Data.Map                        (Map)
+import qualified Data.Map                        as M
+import           Data.Maybe
+import           Data.Monoid
+import           Network.HTTP.Types
+import           Network.Wai
+import           Network.Wai.Handler.Warp        (Port, runSettings,
+                                                  settingsPort)
+import           System.FilePath.Posix
+import           Text.Blaze.Html.Renderer.String as BRS
+import           Text.Blaze.Html.Renderer.Utf8   as BRU
+import           Text.Blaze.Internal
+import           Text.Hastache
+import           Text.Hastache.Context
+import           Web.Scotty                      as S
+import           Web.Scotty.Types
 
 -- | State with Hastache config
-type HState = StateT ((MuConfig IO, Map String (MuType IO))) IO 
-                
+type HState = StateT ((MuConfig IO, Map String (MuType IO))) IO
+
 type ScottyH = ScottyT HState
 type ActionH = ActionT HState
 
@@ -47,7 +45,7 @@ scottyHOpts opts s = do
     putStrLn $ "Setting phasers to stun... (port " ++ show (settingsPort (settings opts)) ++ ") (ctrl-c to quit)"
   runSettings (settings opts) =<< scottyHApp s defaultConfig
 
-  
+
 scottyHApp :: ScottyH () -> MuConfig IO -> IO Application
 scottyHApp defs conf = do
   (s, (muconf,_)) <- runStateT (execStateT (runScottyT defs) def) (conf, mempty)
@@ -57,7 +55,7 @@ scottyHApp defs conf = do
 hroute :: MuConfig IO -- ^ The initial config
        -> MiddlewareT HState
        -> IO Middleware
-hroute conf mw = return $ \app -> 
+hroute conf mw = return $ \app ->
   let (r :: ApplicationT HState) = mw app
   --- ApplicationT HState == Request -> ResourceT HState Response
   in hoist (flip evalStateT (conf, mempty)) . r
@@ -69,19 +67,19 @@ notFoundApp _ = return $ ResponseBuilder status404 [("Content-Type","text/html")
 
 setTemplatesDir :: FilePath -> ScottyH ()
 setTemplatesDir dir = do
-  (conf :: MuConfig IO, map) <- lift State.get
-  lift . State.put $ (conf { muTemplateFileDir = Just dir }, map)
+  (conf :: MuConfig IO, tmap) <- lift State.get
+  lift . State.put $ (conf { muTemplateFileDir = Just dir }, tmap)
 
 setHastacheConfig :: MuConfig IO -> ScottyH ()
 setHastacheConfig conf = do
-  (_, map) <- lift State.get
-  lift . State.put $ (conf, map)
+  (_, tmap) <- lift State.get
+  lift . State.put $ (conf, tmap)
 
 hastache :: FilePath -> ActionT HState ()
 hastache tpl = do
-  ((conf :: MuConfig IO), map) <- lift State.get
+  ((conf :: MuConfig IO), tmap) <- lift State.get
   header "Content-Type" "text/html"
-  let cntx a  = fromMaybe MuNothing (M.lookup a map)
+  let cntx a  = fromMaybe MuNothing (M.lookup a tmap)
   let tplFile = fromMaybe "." (muTemplateFileDir conf)
               </> tpl
               ++ fromMaybe "" (muTemplateFileExt conf)
@@ -90,8 +88,8 @@ hastache tpl = do
 
 setH :: String -> MuType IO -> ActionT HState ()
 setH x y = do
-  (conf, map) <- lift State.get
-  lift . State.put $ (conf, M.insert x y map)
+  (conf, tmap) <- lift State.get
+  lift . State.put $ (conf, M.insert x y tmap)
 
 instance Show Markup where
   show = BRS.renderHtml
